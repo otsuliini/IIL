8f42b9c025044de8324feb6939437db59c5c6a3a
boom1401 <otso.reijonen@icloud.com>
2026-04-26 00:58:13 +0300

    fixed include statement

--- src/parser/errors/errorRoutines.cpp
+++ src/parser/errors/errorRoutines.cpp
@@ -1,12 +1,12 @@
 #include "errorRoutines.hpp"
 #include <memory>
 #include <iostream>
-#include "../src/parser/include/parse.hpp"
+#include "parser/parse.hpp"
 
 // Log error
 
 std::unique_ptr<ExprAST> LogError(const char *Str)
 {
     fprintf(stderr, "Error: %s\n", Str);
-    return nullptr;
-}
+    return nullptr; 
+} 
