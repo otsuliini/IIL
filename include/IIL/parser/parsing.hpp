ba147ca915cfbc77fa2fa77797fd16655f3c386b
boom1401 <otso.reijonen@icloud.com>
2026-05-09 18:44:51 +0300

    added default constructors for OpTok struct

--- include/IIL/parser/parsing.hpp
+++ include/IIL/parser/parsing.hpp
@@ -5,33 +5,55 @@
 
 class numericalExprAST
 {
-    std::shared_ptr<ASTnode> numericalExprAST::parseRHS(const Token number, const Token Oper); 
+    std::shared_ptr<ASTnode> numericalExprAST::parseRHS(const Token number, const Token Oper);
 
 public:
     std::vector<ASTnode> NumExprAST(std::vector<Token> &Tokens);
 };
 
-struct OpTok : Token {
-    Precedence precedence; 
-    Token leftChild; 
-    Token rightChild; 
+struct OpTok : Token
+{
+    Precedence precedence;
+    Token leftChild;
+    Token rightChild;
 
-    OpTok(Token left_child, Token right_child, Token val) {
-        std::unique_ptr<OpTok> ptrTok; 
-        leftChild = left_child; 
-        rightChild = right_child; 
-        value = val.value; 
-        Precedence precednece = Precedence::Undefined; 
+    OpTok(Token left_child, Token right_child, Token val)
+    {
+        std::unique_ptr<OpTok> ptrTok;
+        leftChild = left_child;
+        rightChild = right_child;
+        value = val.value;
+        Precedence precednece = Precedence::Undefined;
     }
 
-    OpTok(Token left_child, Token right_child) {
-        std::unique_ptr<OpTok> ptrTok; 
-        leftChild = left_child; 
-        rightChild = right_child; 
-        Precedence precednece = Precedence::Undefined; 
+    OpTok(Token left_child, Token right_child)
+    {
+        std::unique_ptr<OpTok> ptrTok;
+        leftChild = left_child;
+        rightChild = right_child;
+        Precedence precednece = Precedence::Undefined;
     }
-    OpTok(Token val) {
-        value = val.value; 
+    OpTok(Token val)
+    {
+        value = val.value;
+        leftChild.column = NULL;
+        leftChild.line = NULL;
+        leftChild.type = TokenType::NONE;
+        leftChild.value = " ";
+        rightChild.column = NULL;
+        rightChild.line = NULL;
+        rightChild.type = TokenType::NONE;
+        rightChild.value = " ";
     }
-    
-}; 
+    OpTok()
+    {
+        leftChild.column = NULL;
+        leftChild.line = NULL;
+        leftChild.type = TokenType::NONE;
+        leftChild.value = " ";
+        rightChild.column = NULL;
+        rightChild.line = NULL;
+        rightChild.type = TokenType::NONE;
+        rightChild.value = " ";
+    }
+};
